414ba741c3942fa6534e3d73bcbc538521924140
momin096 <momin09609@gmail.com>
2025-03-09 03:41:59 +0600

    All product sort in desending order function added

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,14 +1,24 @@
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
 
 const AllProducts = () => {
     const products = useLoaderData();
-    console.log(products)
+    const [sortedProducts, setSortedProducts] = useState(products);
+
+    const handleSort = () =>{
+        const sorted = [...sortedProducts].sort((a,b)=> b.price - a.price);
+        setSortedProducts(sorted);
+    }
     return (
         <div className="container mx-auto">
+            <div className="flex justify-between">
+                <div></div>
+                <button onClick={handleSort} className=" my-10 border border-orange-500 px-8 py-2 rounded-lg btn text-xl">Sort</button>
+            </div>
             <div className="grid grid-cols-3 gap-5">
                 {
-                    products.map((product,idx) => <ProductCard key={idx} product={product} />)
+                    sortedProducts.map((product, idx) => <ProductCard key={idx} product={product} />)
                 }
             </div>
 
